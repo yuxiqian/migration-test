1222c9a93c7a01aeae15d2bbd9c9e0b31aa7d100
yuxiqian <34335406+yuxiqian@users.noreply.github.com>
2024-06-18 16:32:10 +0800

    Polish CI scripts

--- misc/patch_flink_conf.rb
+++ misc/patch_flink_conf.rb
@@ -11,3 +11,6 @@ execution.checkpointing.interval: 300
 EXTRACONF
 
 File.write("#{FLINK_HOME}/conf/flink-conf.yaml", EXTRA_CONF, mode: 'a+')
+
+# MySQL connector is not provided
+`wget https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.27/mysql-connector-java-8.0.27.jar -O #{FLINK_HOME}/lib/mysql-connector-java-8.0.27.jar`
